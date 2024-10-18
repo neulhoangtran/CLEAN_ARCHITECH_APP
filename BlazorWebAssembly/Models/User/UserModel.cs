abfc3ea594d321c31fb331728679b6decf9d6677
neul <neulneul2@gmail.com>
2024-10-18 17:01:39 +0700

    update

--- BlazorWebAssembly/Models/User/UserModel.cs
+++ BlazorWebAssembly/Models/User/UserModel.cs
@@ -1,12 +1,38 @@
-﻿namespace BlazorWebAssembly.Models.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAssembly.Models.User
 {
     public class UserModel
     {
+
         public int Id { get; set; }
+
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        public string EmployeeId { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        [Required]
         public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
+
         public string Address { get; set; }
+
+        public string Department { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
     }
 }
